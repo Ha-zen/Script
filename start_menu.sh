364ff5b25ff0133903aa062be50548ac8f63cf48
coulicoulaser <71705200+coulicoulaser@users.noreply.github.com>
2020-09-22 15:14:22 +0200

    V1
    Texte à adapter

--- start_menu.sh
+++ start_menu.sh
@@ -1,1 +1,8 @@
-#start_menu
+#!/bin/bash
+#Debut du script : mise a jour et installation de "dialog"
+
+apt-get update
+apt-get install dialog
+
+#Debut du script de securisation
+dialog --title "INFRASEC" --msgbox 'Bonjour, ce script a pour but blablabla' 20 50
