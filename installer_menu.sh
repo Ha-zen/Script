aaa167094c5b7757b0bc4f162e85286fc4d4bfb0
Ha-zen <porto1768@gmail.com>
2020-09-23 09:16:51 +0200

    commit

--- installer_menu.sh
+++ installer_menu.sh
@@ -1,2 +1,19 @@
+#!/bin/bash/
 
-#test
+# : ${DIALOG_OK=0}
+# : ${DIALOG_CANCEL=1}
+# : ${DIALOG_HELP=2}
+# : ${DIALOG_EXTRA=3}
+# : ${DIALOG_ITEM_HELP=4}
+# : ${DIALOG_ESC=255}
+
+#tmp_file=$(tempfile 2>/dev/null) || tmp_file=/tmp/test$$
+#trap "rm -f $tmp_file" 0 1 2 5 15
+
+
+dialog --title "Installation" --checklist "test" 0 0 0 \
+    1 Attack off \
+    2 Defense on \
+    3 Forensic off \
+    4 Admin off \
+    5 Anonym
