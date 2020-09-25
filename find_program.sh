e3832614ebd912f43dd507cd2e5d1b227695f3f8
Ha-zen <porto1768@gmail.com>
2020-09-25 10:52:10 +0200

    comit

--- find_program.sh
+++ find_program.sh
@@ -1,10 +1,11 @@
 #!/bin/bash/
 # IFS=
-T=$(ps -el | awk '{print $14}' | awk 'NR>2')
-# echo "${T}"
-liste=()
-for value in ${T}
-    do echo "${value}"
-    # liste=(${value})  
-done
-# echo "${liste}"
+liste=("ssh" "nmap")
+# dpkg-query -W -f='${Status}' $liste | grep -q -P '^install ok installed$'; echo $?
+# T=$(ps -el | awk '{print $14}' | awk 'NR>2')
+# # echo "${T}"
+# for value in ${T}
+#     do echo "${value}"
+#     # liste=(${value})  
+# done
+echo "${liste[@]}"
