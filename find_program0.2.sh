4be4d4e64c13f497a3af085bf3fbd0adf6b48b5f
Ha-zen <porto1768@gmail.com>
2020-09-25 11:39:34 +0200

    commit

--- find_program0.2.sh
+++ find_program0.2.sh
@@ -4,7 +4,7 @@ declare -a liste=( "ssh" "nmap" )
 #dpkg-query -W -f='${Status}' $liste | grep -q -P '^install ok installed$'; echo $?
 
 
-for pkg in ${liste[@]}
-    echo "${pkg}"  
+for pkg in ${liste[@]}; do
+    echo ${pkg}  
 done
 echo ${liste[@]}
