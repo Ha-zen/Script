4287f23dd32f83cbae9120639fb49a48d5a16257
Ha-zen <porto1768@gmail.com>
2020-10-06 13:31:57 +0200

    commit

--- find_program0.2.sh
+++ find_program0.2.sh
@@ -6,13 +6,14 @@ declare -a liste=( "openssh-server" "sudo" "nessus" )
 # grep "install ok installed"
 for pkg in ${liste[@]}; do
     
-    check_pkg=$(dpkg-query -W -f='${Status}\n' $pkg | awk '{print $3}' )
-    version_pkg=$(dpkg-query -W -f=' - ${version}' $pkg)
+    check_pkg=$(dpkg-query -W -f='${Status}\n' $pkg | awk '{print $3}' 2&1>/dev/null )
+    version_pkg=$(dpkg-query -W -f=' - ${version}' $pkg 2&1>/dev/null)
     # echo $check_pkg
     
     if [ "${check_pkg}" == "installed" ]; then
         echo -e "$pkg $version_pkg est déjà installé"
     else
+        
         echo "$pkg n'est pas installé"
     fi
 done
