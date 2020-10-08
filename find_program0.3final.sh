ec75431f7a965ebbef9182c6e883f37cf261e4cc
Ha-zen <porto1768@gmail.com>
2020-10-08 16:25:21 +0200

    commit

--- find_program0.3final.sh
+++ find_program0.3final.sh
@@ -32,6 +32,20 @@ echo "vous utilisez $systemis"
 # Résume des programmes
 
 # liste des programmes
+let i=0 # define counting variable
+W=() # define working array
+while read -r line; do # process file by file
+    let i=$i+1
+    W+=($i "$line")
+done < <( more /tmp/liste )
+FILE=$(dialog --title "List file of directory /home" --menu "Chose one" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
+clear
+
+
+
+if [ $? -eq 0 ]; then # Exit with OK
+    readlink -f $(more /tmp/liste | sed -n "echo "$FILE p" | sed 's/ //'")
+fi
 # for i in non_installed_pkg; do
 
 
--- testfile.sh
+++ testfile.sh
@@ -9,6 +9,6 @@ while read -r line; do # process file by file
 done < <( more /tmp/liste )
 FILE=$(dialog --title "List file of directory /home" --menu "Chose one" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
 clear
-if [ $? -eq 0 ]; then # Exit with OK
-    readlink -f $(more /tmp/liste | sed -n "echo "$FILE p" | sed 's/ //'")
-fi
+# if [ $? -eq 0 ]; then # Exit with OK
+#     readlink -f $(more /tmp/liste | sed -n "echo "$FILE p" | sed 's/ //'")
+# fi
