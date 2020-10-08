1aacca494d43ffdcd86e934387a5588c4842d609
Ha-zen <porto1768@gmail.com>
2020-10-08 16:18:22 +0200

    commit

--- testfile.sh
+++ testfile.sh
@@ -6,9 +6,9 @@ W=() # define working array
 while read -r line; do # process file by file
     let i=$i+1
     W+=($i "$line")
-done < <( more /tmp/non_installed_pkg )
+done < <( more /tmp/liste )
 FILE=$(dialog --title "List file of directory /home" --menu "Chose one" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
 clear
 if [ $? -eq 0 ]; then # Exit with OK
-    readlink -f $(more /tmp/non_installed_pkg | sed -n "echo "$FILE p" | sed 's/ //'")
+    readlink -f $(more /tmp/liste | sed -n "echo "$FILE p" | sed 's/ //'")
 fi
