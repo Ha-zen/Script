f0e7277e9cabab7749aafbd40e1b2c6f698a2fce
Ha-zen <porto1768@gmail.com>
2020-10-09 10:56:27 +0200

    commit

--- testfile.sh
+++ testfile.sh
@@ -7,7 +7,7 @@ while read -r line; do # process file by file
     let i=$i+1
     W+=($i "$line")
 done < <( more /tmp/liste )
-FILE=$(dialog --title "List file of directory /home" --menu "Chose one" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
+FILE=$(dialog --title "List file of directory /home" --radiolist "Choose one" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
 clear
 # if [ $? -eq 0 ]; then # Exit with OK
 #     readlink -f $(more /tmp/liste | sed -n "echo "$FILE p" | sed 's/ //'")
